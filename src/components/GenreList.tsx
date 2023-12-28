e8a54df9de7b3ed62c2878948caa32373035c9e5
Younes-ch <younes.jmell10@gmail.com>
2023-12-28 01:44:07 +0100

    Create a generic data fetching hook.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import useGenres from '../hooks/useGenres'
+import React from "react";
+import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-    const { genres } = useGenres();
+  const { data } = useGenres();
 
-    return (
-        <ul>
-            {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-      </ul>
-  )
-}
+  return (
+    <ul>
+      {data.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
 
-export default GenreList
+export default GenreList;
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -18,22 +18,26 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-    const iconMap: { [key: string]: IconType} = {
-        pc: FaWindows,
-        playstation: FaPlaystation,
-        xbox: FaXbox,
-        mac: FaApple,
-        linux: FaLinux,
-        android: FaAndroid,
-        ios: MdPhoneIphone,
-        nintendo: SiNintendo,
-        web: BsGlobe
-    }
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
 
   return (
     <HStack marginY={1}>
-          {platforms.map((platform) => (
-          <Icon as={iconMap[platform.slug]} color={'gray.500'}/>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
       ))}
     </HStack>
   );

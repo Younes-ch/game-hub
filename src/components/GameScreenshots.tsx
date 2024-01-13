fa1ce615fa913f3e9dc87419d962caa6d812138d
Younes-ch <younes.jmell10@gmail.com>
2024-01-13 15:06:18 +0100

    Show loading state of screenshots.

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -1,4 +1,4 @@
-import { Image, SimpleGrid } from "@chakra-ui/react";
+import { Image, SimpleGrid, Skeleton } from "@chakra-ui/react";
 import useScreenshots from "../hooks/useScreenshots";
 
 interface Props {
@@ -7,17 +7,20 @@ interface Props {
 
 const GameScreenshots = ({ gameId }: Props) => {
   const { data, error, isLoading } = useScreenshots(gameId);
-
-  if (isLoading) return null;
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+  if (isLoading)
+    return (
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={6} marginY={5}>
+        {skeletons.map((skeleton) => (
+          <Skeleton key={skeleton} height="200px" />
+        ))}
+      </SimpleGrid>
+    );
 
   if (error) throw error;
 
   return (
-    <SimpleGrid
-      columns={{ base: 1, md: 2}}
-      spacing={6}
-      marginY={5}
-    >
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={6} marginY={5}>
       {data?.results.map((file) => (
         <Image key={file.id} src={file.image} />
       ))}
